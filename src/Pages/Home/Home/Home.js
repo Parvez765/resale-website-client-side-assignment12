9913239e753df9fef76b3b97b2bb500d8d81c80b
Pervez <pervezhossain1998@gmail.com>
2022-11-23 21:55:12 +0600

    Header Addedd

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import headerImg from "../../../assests/images/headerImg-01.png"
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className=' bg-gray-100 p-4'>
+            <div className='flex items-center flex-col lg:flex-row-reverse justify-center'>
+                <div>
+                    <img src={headerImg} className="w-[800px]" alt="" />
+                </div>
+                <div>
+                    <h2 className='text-4xl lg:text-6xl font-bold'>Buy And Sell</h2>
+                    <h2 className='text-2xl font-bol mt-2'>WhatEver You Want, Whenever You Want</h2>
+                    <button className='btn btn-primary mt-5'>Book Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logoIcon from "../../../src/assests/images/logo-01.png"
 
 const Navbar = () => {
     return (
@@ -10,7 +11,11 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <div className='flex items-center'>
+                          
+                            <div><Link className="btn btn-ghost normal-case text-xl">BuyandSell.com</Link></div>
+                        </div>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="">Dashboard</Link></li>
                         <li><Link to="">Blog</Link></li>
@@ -19,7 +24,10 @@ const Navbar = () => {
                    
                     </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">BuyandSell.com</a>
+                    <div className='hidden lg:flex items-center '>
+                        <div><img src={logoIcon} className="w-[80px]" alt="" /></div>
+                        <div><Link className="btn btn-ghost normal-case text-xl">BuyandSell.com</Link></div>
+                    </div>
                 </div>
                 <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
