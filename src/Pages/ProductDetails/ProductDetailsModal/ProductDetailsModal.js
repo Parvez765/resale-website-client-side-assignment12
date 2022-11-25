ef62ea8212d76da232acc276c8921db5b8b99567
Pervez <pervezhossain1998@gmail.com>
2022-11-25 18:52:25 +0600

    Alert for Booking Successfull

--- src/Pages/ProductDetails/ProductDetailsModal/ProductDetailsModal.js
+++ src/Pages/ProductDetails/ProductDetailsModal/ProductDetailsModal.js
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext, useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 
 const ProductDetailsModal = ({ booking}) => {
     const { user } = useContext(AuthContext)
 
-    const { originalPrice, sellingPrice, productDescription, usagesTime, postingTime, location, conditions, productName} = booking
+    const { originalPrice, sellingPrice, productDescription, usagesTime, postingTime, location, conditions, productName, _id} = booking
     
     console.log("This is",booking)
 
@@ -21,6 +22,14 @@ const ProductDetailsModal = ({ booking}) => {
         
         
     })
+
+    const handleBookProduct = () => {
+        Swal.fire(
+            'Congratulation!',
+            'Product Booking Successfull!',
+            'success'
+          )
+    }
 
   
     return (
@@ -42,7 +51,7 @@ const ProductDetailsModal = ({ booking}) => {
                     <p className="py-4">Product Selling Price: {sellingPrice} BDT</p>
                     <p className="py-4">Product Usages Time: {usagesTime}</p>
                     <p className="py-4">Condition: {conditions}</p>
-                   <button className='btn btn-primary'>Submit</button>
+                   <button onClick={handleBookProduct} className='btn btn-primary'>Book Your Desire Product</button>
             </div>
             </div>
         </div>
